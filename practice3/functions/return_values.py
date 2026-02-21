ba82117e97df463e71052dd1b7742a57e073034a
AruzhanBersinbayeva <kusuosajki11@gmail.com>
2026-02-21 12:18:03 +0500

    Add functions for full name and maximum number

---
+++ practice3/functions/return_values.py
@@ -0,0 +1,13 @@
+def get_full_name(first, last):
+    return first + " " + last
+
+print(get_full_name("Dana", "Smith"))
+
+
+
+def max_number(a, b):
+    if a > b:
+        return a
+    return b
+
+print(max_number(10, 25))
