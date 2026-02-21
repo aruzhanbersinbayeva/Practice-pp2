9ffb7bba27c377c37a64fc946014e348c3a3ab26
AruzhanBersinbayeva <kusuosajki11@gmail.com>
2026-02-21 12:21:06 +0500

    Update super_function.py

---
+++ practice3/inheritance:/super_function.py
@@ -0,0 +1,26 @@
+# Example 1
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
+d = Dog("Rex", "Bulldog")
+print(d.name, d.breed)
+
+
+# Example 2
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+class Student(Person):
+    def __init__(self, name, grade):
+        super().__init__(name)
+        self.grade = grade
+
+s = Student("Ali", 9)
+print(s.name, s.grade)
